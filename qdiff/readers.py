7281a44fa752fb0154a74156689d25cee5b8cbc3
Je-Ming Lin <lin.13k@gmail.com>
2018-05-31 19:51:22 -0700

    finish bugfix and testcase for database reader

--- qdiff/readers.py
+++ qdiff/readers.py
@@ -4,10 +4,10 @@ from django.db import connections
 
 class DataReader:
 
-    def readRow(self):
+    def getRow(self):
         raise NotImplementedException("readRow method is not implemented")
 
-    def getIterator(self):
+    def getRowsList(self):
         raise NotImplementedException("getIterator method is not implemented")
 
     def close(self):
@@ -16,29 +16,41 @@ class DataReader:
 
 class DatabaseReader(DataReader):
 
-    def __init__(self, configDict, query_sql):
-        # TODO valid the configDict
-        self.configDict = configDict
-        self.label = 'db_' + str(hash(tuple(sorted(configDict.items()))))
+    def __init__(self, config_dict, query_sql):
+        # TODO valid the config_dict
+        self.config_dict = config_dict
+        if 'id' in config_dict:
+            self.label = config_dict['id']
+        else:
+            self.label = 'db_' + str(hash(tuple(sorted(config_dict.items()))))
         self.query_sql = query_sql
 
+    def getCursor(self):
+        c = connections[self.label].cursor()
+        c.execute(self.query_sql)
+        return c
+
     def register(self):
-        connections._databases[self.label] = self.configDict
+        connections._databases[self.label] = self.config_dict
         del connections.databases
 
-    def readRow(self):
+    def getRow(self):
         if self.label not in connections:
             self.register()
-        if not hasattr(self, 'iterator'):
-            self.iterator = self.getIterator()
-        return next(self.iterator)
-
-    def getIterator(self):
-        if self.label not in connections:
-            self.register()
-        self.cursor = connections[self.label].cursor()
         try:
-            self.cursor.execute(self.query_sql)
+            if not hasattr(self, 'cursor'):
+                self.cursor = self.getCursor()
+        except Exception as e:
+            self.cursor.close()
+            raise e
+        return self.cursor.fetchone()
+
+    def getRowsList(self):
+        if self.label not in connections:
+            self.register()
+        try:
+            if not hasattr(self, 'cursor'):
+                self.cursor = self.getCursor()
         except Exception as e:
             self.cursor.close()
             raise e
---
+++ qdiff/test/test_readers.py
@@ -0,0 +1,69 @@
+from django.test import TestCase
+from django.db import connection, connections
+from random import randint
+from qdiff.readers import DatabaseReader
+
+
+class DatabaseReaderTestCase(TestCase):
+
+    def setUp(self):
+
+        with connection.cursor() as cursor:
+            # create table
+            cursor.execute('''CREATE TABLE temp (
+                        col1 VARCHAR(10),
+                        col2 VARCHAR(10)
+                    );
+                ''')
+
+            # insert data
+            query = '''INSERT INTO temp (col1, col2)
+                    VALUES (%s, %s)
+                '''
+            cursor.executemany(query, [
+                (
+                    str(randint(0, 1)),
+                    str(randint(0, 10))
+                )
+                for i in range(10)])
+
+    def testGetRow(self):
+        newDBConfig = {}
+        newDBConfig['id'] = 'default'
+        newDBConfig['ENGINE'] = 'django.db.backends.sqlite3'
+        newDBConfig['NAME'] = ':memory:'
+        query_sql = 'SELECT * FROM temp;'
+        r = DatabaseReader(newDBConfig, query_sql)
+        row = r.getRow()
+        print(row)
+        r.close()
+
+    '''
+    this require additional mysql setup
+    '''
+    '''
+    def testGetRowsListWithMysql(self):
+        newDBConfig = {}
+        newDBConfig['id'] = 'new'
+        newDBConfig['ENGINE'] = 'django.db.backends.mysql'
+        newDBConfig['NAME'] = 'test2'
+        newDBConfig['PASSWORD'] = 'root'
+        newDBConfig['USER'] = 'root'
+        newDBConfig['HOST'] = 'localhost'
+        newDBConfig['PORT'] = '3306'
+        query_sql = 'SELECT * FROM group2;'
+        r = DatabaseReader(newDBConfig, query_sql)
+        rows = r.getRowsList()
+        self.assertEquals(2, len(rows))
+    '''
+
+    def testGetRowsListWithSqlite(self):
+        newDBConfig = {}
+        newDBConfig['id'] = 'default'
+        newDBConfig['ENGINE'] = 'django.db.backends.sqlite3'
+        newDBConfig['NAME'] = ':memory:'
+        query_sql = 'SELECT * FROM temp;'
+        r = DatabaseReader(newDBConfig, query_sql)
+        rows = r.getRowsList()
+        self.assertEquals(10, len(rows))
+        r.close()
