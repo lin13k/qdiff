f71593f37cb1c2378a15dc99724082530bbbc358
Je-Ming Lin <lin.13k@gmail.com>
2018-06-01 13:05:51 -0700

    rewrite comparator in non-iterator way, finish testing

--- qdiff/comparator.py
+++ qdiff/comparator.py
@@ -9,45 +9,48 @@ class Comparator:
         self.writer1 = writer1
         self.writer2 = writer2
 
-    # TODO, apply ignore_fields in hash
     def compare(self):
-        iter1 = iter(sorted(self.data_reader1.getRowsList()))
-        iter2 = iter(sorted(self.data_reader2.getRowsList()))
+        data_list1 = sorted(self.data_reader1.getRowsList())
+        data_list2 = sorted(self.data_reader2.getRowsList())
+        index1, index2 = 0, 0
         temp_dict1 = {}
         temp_dict2 = {}
-        item1 = None
-        item2 = None
-        try:
-            while True:
-                item1 = tuple(next(iter1))
-                item2 = tuple(next(iter2))
-                # TODO, apply ignore_fields in hash
-                h1 = hash(item1)
-                h2 = hash(item2)
-                if h1 == h2:
-                    item1 = None
-                    item2 = None
-                    continue
-                elif h1 in temp_dict2 or h2 in temp_dict1:
-                    if h1 in temp_dict2:
-                        temp_dict2.pop(h1)
-                        self.writer2.writeAll(temp_dict2.values())
-                    if h2 in temp_dict1:
-                        temp_dict1.pop(h2)
-                        self.writer1.writeAll(temp_dict1.values())
-                else:
-                    temp_dict1[h1] = item1
-                    temp_dict2[h2] = item2
+        while index1 < len(data_list1) and index2 < len(data_list2):
+            item1 = data_list1[index1]
+            item2 = data_list2[index2]
 
-        except StopIteration as e:
-            if item1:
-                # iter1 is empty
-                self.writer1.writeAll([item1] + temp_dict1.values())
+            # TODO, apply ignore_fields in hash
+            h1 = hash(item1)
+            h2 = hash(item2)
+            if h1 == h2:
+                index1 += 1
+                index2 += 1
+            elif h1 in temp_dict2 or h2 in temp_dict1:
+                if h1 in temp_dict2:
+                    temp_dict2.pop(h1)
+                    self.writer2.writeAll(temp_dict2.values())
+                    temp_dict2 = {}
+                    index1 += 1
+                if h2 in temp_dict1:
+                    temp_dict1.pop(h2)
+                    self.writer1.writeAll(temp_dict1.values())
+                    temp_dict1 = {}
+                    index2 += 1
             else:
-                self.writer1.writeAll(temp_dict1.values())
+                temp_dict1[h1] = item1
+                temp_dict2[h2] = item2
+                index1 += 1
+                index2 += 1
 
-            if item2:
-                # iter2 is empty
-                self.writer2.writeAll([item2] + temp_dict2.values())
-            else:
-                self.writer2.writeAll(temp_dict2.values())
+        if len(temp_dict2) > 0:
+            self.writer2.writeAll(temp_dict2.values())
+            temp_dict2 = {}
+        if len(temp_dict1) > 0:
+            self.writer1.writeAll(temp_dict1.values())
+            temp_dict1 = {}
+
+        if index1 != len(data_list1):
+            self.writer1.writeAll(data_list1[index1:])
+
+        if index2 != len(data_list2):
+            self.writer2.writeAll(data_list2[index2:])
---
+++ qdiff/test/test_comparator.py
@@ -0,0 +1,126 @@
+from django.test import TestCase
+from django.db import connection
+from random import randint
+from logging import Logger
+from qdiff.comparator import Comparator
+from qdiff.readers import DatabaseReader
+from qdiff.writers import DatabaseWriter
+
+
+class TestWriter:
+    def __init__(self):
+        self.data = []
+
+    def writeAll(self, rows):
+        self.data.extend(rows)
+
+
+class TestReader:
+    def __init__(self, data):
+        self.data = data
+
+    def getRowsList(self):
+        return self.data
+
+
+class ComparatorTestCase(TestCase):
+    def setUp(self):
+        # setup database for readers
+        with connection.cursor() as cursor:
+            # create table
+            cursor.execute('''CREATE TABLE r1 (
+                        col1 VARCHAR(10),
+                        col2 VARCHAR(10)
+                    );
+                ''')
+            # insert data
+            query = '''INSERT INTO r1 (col1, col2)
+                    VALUES (%s, %s)
+                '''
+            cursor.executemany(query, [
+                (
+                    str(i),
+                    str(i)
+                )
+                for i in range(10)])
+            cursor.execute('''CREATE TABLE r2 (
+                        col1 VARCHAR(10),
+                        col2 VARCHAR(10)
+                    );
+                ''')
+            # insert data
+            query = '''INSERT INTO r2 (col1, col2)
+                    VALUES (%s, %s)
+                '''
+            cursor.executemany(query, [
+                (
+                    str(i),
+                    str(i)
+                )
+                for i in range(11)])
+        # setup database for writers
+        with connection.cursor() as cursor:
+            cursor.execute('''CREATE TABLE w1 (
+                        col1 VARCHAR(10),
+                        col2 VARCHAR(10)
+                    );
+                ''')
+            cursor.execute('''CREATE TABLE w2 (
+                        col1 VARCHAR(10),
+                        col2 VARCHAR(10)
+                    );
+                ''')
+
+    def testSameDataComparisonWithDatabase(self):
+        DbConfig = {}
+        DbConfig['id'] = 'default'
+        DbConfig['ENGINE'] = 'django.db.backends.sqlite3'
+        DbConfig['NAME'] = ':memory:'
+        query_sql1 = 'SELECT * FROM r1;'
+        r1 = DatabaseReader(DbConfig, query_sql1)
+        query_sql2 = 'SELECT * FROM r2;'
+        r2 = DatabaseReader(DbConfig, query_sql2)
+
+        w1 = DatabaseWriter(connection, 'w1')
+        w2 = DatabaseWriter(connection, 'w2')
+
+        comparator = Comparator(r1, r2, w1, w2, [])
+        comparator.compare()
+
+        with connection.cursor() as cursor:
+            cursor.execute('SELECT * FROM w1;')
+            records1 = list(cursor.fetchall())
+            cursor.execute('SELECT * FROM w2;')
+            records2 = list(cursor.fetchall())
+            self.assertEqual(len(records1), 0)
+            self.assertEqual(len(records2), 1)
+
+    def testDataLogic1(self):
+        r1 = TestReader([1, 2, 3, 4, 5])
+        r2 = TestReader([1, 2, 3, 4, 5])
+        w1 = TestWriter()
+        w2 = TestWriter()
+        comparator = Comparator(r1, r2, w1, w2)
+        comparator.compare()
+        self.assertEqual(len(w1.data), 0)
+        self.assertEqual(len(w2.data), 0)
+
+    def testDataLogic2(self):
+        r1 = TestReader([1, 2, 4, 5])
+        r2 = TestReader([1, 2, 3, 4, 5])
+        w1 = TestWriter()
+        w2 = TestWriter()
+        comparator = Comparator(r1, r2, w1, w2)
+        comparator.compare()
+        self.assertEqual(len(w1.data), 0)
+        self.assertEqual(len(w2.data), 1)
+
+    def testDataLogic3(self):
+        r1 = TestReader([1, 2, 4, 5, 6, 8])
+        r2 = TestReader([1, 2, 3, 4, 5, 9])
+        w1 = TestWriter()
+        w2 = TestWriter()
+        comparator = Comparator(r1, r2, w1, w2)
+        comparator.compare()
+        self.assertEqual(len(w1.data), 2)
+        self.assertEqual(len(w2.data), 2)
