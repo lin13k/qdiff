15c69b2dff998e756dc47ded6e29f812677b21e7
Je-Ming Lin <lin.13k@gmail.com>
2018-06-05 16:09:03 -0700

    bug fix, sometimes the config contains nested dict

--- qdiff/abstracts.py
+++ qdiff/abstracts.py
@@ -7,7 +7,7 @@ class AbstractDatabaseAccessUnit:
         if 'id' in config_dict:
             self.label = config_dict['id']
         else:
-            self.label = 'db_' + str(hash(tuple(sorted(config_dict.items()))))
+            self.label = 'db_' + str(hash(str(sorted(config_dict.items()))))
 
     def register(self):
         connections._databases[self.label] = self.config_dict
