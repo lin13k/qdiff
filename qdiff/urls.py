57be39333766105cebc4a3b9d9d7ecd33caa0c7a
Je-Ming Lin <lin.13k@gmail.com>
2018-06-25 17:07:22 -0700

    wip

--- qdiff/urls.py
+++ qdiff/urls.py
@@ -4,5 +4,8 @@ from qdiff import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    url(r'^tasks/?$', views.task_list_view, name='task_list')
+    url(r'^tasks/?$',
+        views.task_list_view, name='task_list'),
+    url(r'^tasks?/(?P<pk>[0-9]+)/?$',
+        views.task_detail_view, name='task_detail'),
 ]
--- qdiff/views.py
+++ qdiff/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
-from qdiff.models import Task
-
+from django.shortcuts import render, get_object_or_404
+from qdiff.models import Task, ConflictRecord
+from qdiff.readers import DatabaseReader
+from django.conf import settings
 
 
 def task_list_view(request):
@@ -10,3 +10,35 @@ def task_list_view(request):
     taskList.reverse()
     context['tasks'] = taskList
     return render(request, 'qdiff/task_list.html', context)
+
+
+def task_detail_view(request, pk):
+    context = {}
+    task = get_object_or_404(Task, id=pk)
+    defaultConfigs = settings.DATABASES['default'].copy()
+    tableName1 = '%s_TASK_%s_%s' % (
+        settings.GENERATED_TABLE_PREFIX,
+        str(task.id),
+        ConflictRecord.POSITION_IN_TASK_LEFT
+    )
+    tableName2 = '%s_TASK_%s_%s' % (
+        settings.GENERATED_TABLE_PREFIX,
+        str(task.id),
+        ConflictRecord.POSITION_IN_TASK_RIGHT
+    )
+    datareader1 = DatabaseReader(
+        defaultConfigs,
+        'SELECT * FROM %s;' % (tableName1))
+    result1 = [(*item, ConflictRecord.POSITION_IN_TASK_LEFT)
+               for item in datareader1.getRowsList()]
+    datareader2 = DatabaseReader(
+        defaultConfigs,
+        'SELECT * FROM %s;' % (tableName2))
+    result2 = [(*item, ConflictRecord.POSITION_IN_TASK_RIGHT)
+               for item in datareader2.getRowsList()]
+    conflictResults = result1 + result2
+    columns = datareader1.getColumns()
+    context['task'] = task
+    context['columns'] = columns
+    context['conflictResults'] = conflictResults
+    return render(request, 'qdiff/task_detail.html', context)
