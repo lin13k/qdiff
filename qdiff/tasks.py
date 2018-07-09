50bd5207ea61064463a1131e4df687e48ad652da
Je-Ming Lin <lin.13k@gmail.com>
2018-07-09 15:09:26 -0700

    add more detail when casterror happened

--- qdiff/tasks.py
+++ qdiff/tasks.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import, unicode_literals
 from celery.decorators import task
 from qdiff.models import Task
 from qdiff.managers import TaskManager
+from tableschema.exceptions import CastError
+import traceback
 
 
 @task(name="test_task")
@@ -24,8 +26,11 @@ def compareCommand(
             wds2)
         manager.compare()
     except Exception as e:
+        # traceback.print_exc(e)
         model.result = Task.STATUS_OF_TASK_ERROR
         model.result_detail = str(e)
+        if hasattr(e, 'errors'):
+            model.result_detail = str(e) + ':' + ' '.join(map(str, e.errors))
         model.save()
     finally:
         # delete db config
