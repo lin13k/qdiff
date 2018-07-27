77bfbf7b6b60ff0c876fbaaf3970b4331b44407a
Je-Ming Lin <lin.13k@gmail.com>
2018-07-27 15:54:46 -0700

    change name of task_id to taskId

--- qdiff/urls.py
+++ qdiff/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from qdiff import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    url(r'^$',
+        views.task_list_view, name='index'),
     url(r'^tasks/?$',
         views.task_list_view, name='task_list'),
     url(r'^tasks?/(?P<pk>[0-9]+)/?$',
@@ -16,17 +17,19 @@ urlpatterns = [
         name='create_config_file_upload'),
 
     # statics pie report
-    url(r'^statics_pie_reports?/(?P<task_id>\d+)/?$',
+    url(r'^statics_pie_reports?/(?P<taskId>\d+)/?$',
         views.statics_pie_report_view, name='static_pie_report'),
 
-    # aggregated report
-    url(r'^aggregated_reports?/(?P<task_id>\d+)/api/?$',
-        views.Agregated_Report_APIView.as_view(),
-        name='AggregatedReportGenerator_api'),
-    url(r'^aggregated_reports?/(?P<task_id>\d+)/?$',
+    # aggregated report page
+    url(r'^aggregated_reports?/(?P<taskId>\d+)/?$',
         views.aggregated_report_view, name='AggregatedReportGenerator'),
     #                                 name from class
-    url(r'^aggregated_reports?/(?P<task_id>\d+)/download/?$',
+    # aggregated report data content
+    url(r'^aggregated_reports?/(?P<taskId>\d+)/api/?$',
+        views.Agregated_Report_APIView.as_view(),
+        name='AggregatedReportGenerator_api'),
+    # aggregated report csv download
+    url(r'^aggregated_reports?/(?P<taskId>\d+)/download/?$',
         views.Agregated_Report_CSV_Download_APIVIEW.as_view(),
         name='AggregatedReportGenerator_download'),
 ]
